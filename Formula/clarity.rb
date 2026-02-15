e8ee019d8402869ae87542c5409e628950f4e301
Prerak Gada <58708396+PrerakGada@users.noreply.github.com>
2026-02-15 11:32:39 +0530

    clarity cli-v0.1.0-alpha.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/clarity.rb
+++ Formula/clarity.rb
@@ -1,8 +1,8 @@
 class Clarity < Formula
   desc "CLI for Clarity Engine — think with AI agent teams"
   homepage "https://github.com/PrerakGada/clarity_engine"
-  url "https://github.com/PrerakGada/clarity_engine/releases/download/cli-v0.1.0-alpha.1/clarity-0.1.0-alpha.1.tar.gz"
-  sha256 "09692e6b770f48a0c2b3d3fdf11f627a3c4a66898a071c211fb09cd20e3d4f38"
+  url "https://github.com/PrerakGada/clarity_engine/releases/download/cli-v0.1.0-alpha.2/clarity-0.1.0-alpha.2.tar.gz"
+  sha256 "a77cdb016273e0d44321d005c8f6d40c6736361f1dfcabd542996eefbe1d5c43"
   license "MIT"
 
   depends_on "node@22"
